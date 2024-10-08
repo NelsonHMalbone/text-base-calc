c219ffc32b54ab9dfd87f965a2935e284b390f7d
Nelson Malbone <nhmalbone@icloud.com>
2024-10-08 18:16:23 -0400

    10-8-2024 DailyPython Project Beginner Level
    added a sys exit so user can exit program along with a way to continue using program.

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
+import sys
 # Daily Python Project
 # text base calculator
+while True:
+    # have user choose a operator
+    function_input = input("Enter Operator + - * / or 'exit' : ")
+    if function_input == 'exit':
+        sys.exit()
+    else:
+        # Have user input two numbers
+        user_number_input_1 = int(input('enter first number: '))
+        user_number_input_2 = int(input('enter second number: '))
 
-# Have user input two numbers
-user_number_input_1 = int(input('enter first number: '))
-user_number_input_2 = int(input('enter second number: '))
+        match function_input:
+            case '+':
+                result_Add = user_number_input_1 + user_number_input_2
+                print(result_Add)
+            case '-':
+                result_sub = user_number_input_1-user_number_input_2
+                print(result_sub)
+            case '*':
+                result_mult = user_number_input_1 * user_number_input_2
+                print(result_mult)
 
-# have user choose a operator
-function_input = input("Enter Operator + - * /: ")
-
-
-match function_input:
-    case '+':
-        result_Add = user_number_input_1 + user_number_input_2
-        print(result_Add)
-    case '-':
-        result_sub = user_number_input_1-user_number_input_2
-        print(result_sub)
-    case '*':
-        result_mult = user_number_input_1 * user_number_input_2
-        print(result_mult)
-
-    case '/':
-        try:
-            result_divison = user_number_input_1 / user_number_input_2
-            print(result_divison)
-        except ZeroDivisionError:
-            print("Error message: Divison by Zero: Can not divide by Zero")
+            case '/':
+                try:
+                    result_divison = user_number_input_1 / user_number_input_2
+                    print(result_divison)
+                except ZeroDivisionError:
+                    print("Error message: Divison by Zero: Can not divide by Zero")
