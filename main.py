7294fd39b48a11afb590b0525229bb0a214f4349
Nelson Malbone <nhmalbone@icloud.com>
2024-10-08 07:59:30 -0400

    10-8-2024 DailyPython Project Beginner Level
    place a error message for divison of 0
    each line has its own print statement
    the other way was not working so diverted to each case having its own print line

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ user_number_input_2 = int(input('enter second number: '))
 # have user choose a operator
 function_input = input("Enter Operator + - * /: ")
 
+
 match function_input:
     case '+':
-        result = user_number_input_1 + user_number_input_2
+        result_Add = user_number_input_1 + user_number_input_2
+        print(result_Add)
     case '-':
-        result = user_number_input_1-user_number_input_2
+        result_sub = user_number_input_1-user_number_input_2
+        print(result_sub)
     case '*':
-        result = user_number_input_1 * user_number_input_2
-    case '/':
-        result = user_number_input_1 / user_number_input_2
+        result_mult = user_number_input_1 * user_number_input_2
+        print(result_mult)
 
-print(result)
+    case '/':
+        try:
+            result_divison = user_number_input_1 / user_number_input_2
+            print(result_divison)
+        except ZeroDivisionError:
+            print("Error message: Divison by Zero: Can not divide by Zero")
