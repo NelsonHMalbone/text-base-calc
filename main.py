d838adf2225629e92598c6aaf84cc70cc9a64c05
Nelson Malbone <nhmalbone@icloud.com>
2024-10-08 07:40:39 -0400

    10-8-2024 DailyPython Project Beginner Level
    using a match case method. having user input two numbers then a operator to get a result.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 # Daily Python Project
 # text base calculator
 
+# Have user input two numbers
 user_number_input_1 = int(input('enter first number: '))
 user_number_input_2 = int(input('enter second number: '))
 
-result = user_number_input_1 + user_number_input_2
+# have user choose a operator
+function_input = input("Enter Operator + - * /: ")
+
+match function_input:
+    case '+':
+        result = user_number_input_1 + user_number_input_2
+    case '-':
+        result = user_number_input_1-user_number_input_2
+    case '*':
+        result = user_number_input_1 * user_number_input_2
+    case '/':
+        result = user_number_input_1 / user_number_input_2
+
 print(result)
