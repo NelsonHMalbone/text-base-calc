925a25cf36f5eac7c777a8bb4780f6bc31449e4f
Nelson Malbone <nhmalbone@icloud.com>
2024-10-08 07:29:13 -0400

    10-8-2024 DailyPython Project Beginner Level
    hard code additon to see if inputs are functional in correct

---
+++ main.py
@@ -0,0 +1,8 @@
+# Daily Python Project
+# text base calculator
+
+user_number_input_1 = int(input('enter first number: '))
+user_number_input_2 = int(input('enter second number: '))
+
+result = user_number_input_1 + user_number_input_2
+print(result)
